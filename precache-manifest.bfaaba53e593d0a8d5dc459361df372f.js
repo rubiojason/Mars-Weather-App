1ade324e696a97241d435fa429443b0d285bcfd2
Jason Rubio <jason@Denises-iMac.local>
2020-10-21 07:55:03 -0700

    Updates

---
+++ precache-manifest.bfaaba53e593d0a8d5dc459361df372f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bc329cb6f505a4094497b57c2a1949",
+    "url": "/Mars-Weather-App/index.html"
+  },
+  {
+    "revision": "d46af9f92b703cbeb62f",
+    "url": "/Mars-Weather-App/static/css/main.0eb0be1a.chunk.css"
+  },
+  {
+    "revision": "bfd570daa82e55b457c6",
+    "url": "/Mars-Weather-App/static/js/2.69286f0f.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/Mars-Weather-App/static/js/2.69286f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46af9f92b703cbeb62f",
+    "url": "/Mars-Weather-App/static/js/main.35e5a082.chunk.js"
+  },
+  {
+    "revision": "b0ad0934906ad09b0037",
+    "url": "/Mars-Weather-App/static/js/runtime-main.a16c8bdc.js"
+  }
+]);
